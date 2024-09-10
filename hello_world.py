8f0bc0f5ca78a1031c389af2e8ea2e0deba32b9d
sem1n <songsm1129@gmail.com>
2024-09-10 12:32:55 +0900

    Update2 hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,7 @@
-def print_hello():
-    print("Hello World!!")
+print('Hello World!')
 
 def print_KIT():
-    print("Kumoh National INstitute of Technology.")
+    print('KIT')
 
 if __name__=='__main__':
     print_KIT()
-    print_hello()
